086f5c60d65c0e18e8ad84dfaa196c925a05d4d0
Maxim <loznyan84@gmail.com>
2024-07-19 17:29:47 +0300

    перша домашка

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./FriendListItem.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <div className={styles.friend}>
+    <span className={isOnline ? styles.online : styles.offline}></span>
+    <img src={avatar} alt="Avatar" width="48" className={styles.avatar} />
+    <p className={styles.name}>{name}</p>
+  </div>
+);
+
+export default FriendListItem;
